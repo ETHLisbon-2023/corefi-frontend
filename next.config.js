918791e43c9bbdf0f354773e41b99b4e85a0c60d
Shashkov Danil <shashkovdanil@gmail.com>
2023-11-03 22:26:05 +0000

    add domain config

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
     config.externals.push('pino-pretty', 'lokijs', 'encoding')
     return config
   },
+  experimental: {
+    webpackBuildWorker: true,
+  },
 }
 
 module.exports = nextConfig
