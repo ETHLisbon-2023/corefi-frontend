9d528979a529401225dd177dafcac74d835ac508
Shashkov Danil <shashkovdanil@gmail.com>
2023-11-03 20:42:45 +0000

    use walletconnect hooks

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -1,11 +1,22 @@
 import { Button } from '@/components/ui/button'
+import { useWeb3Modal } from '@web3modal/wagmi/react'
+import { useAccount } from 'wagmi'
 
 export function Header() {
+  const { open } = useWeb3Modal()
+  const { address, isConnecting } = useAccount()
+
   return (
     <header>
       <div></div>
       <div>
-        <w3m-button />
+        {address ? (
+          <w3m-account-button />
+        ) : (
+          <Button onClick={() => open()} type="button">
+            {isConnecting ? 'Connecting...' : 'Connect Wallet'}
+          </Button>
+        )}
       </div>
     </header>
   )
